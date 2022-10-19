0b6aaa30c39f5d8587971925dbf58bc8afb2a612
abialeem <aabidaleem143@gmail.com>
2022-10-19 17:52:05 +0530

    order history + mini cart bar update 1

--- src/app/order-history-item/order-history-item.component.ts
+++ src/app/order-history-item/order-history-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AddressService } from '../services/address.service';
+import { ApiService } from '../services/api.service';
+import { ProductService } from '../services/product.service';
 import { Address } from '../shared/models/address.model';
 
 @Component({
@@ -31,22 +33,57 @@ export class OrderHistoryItemComponent implements OnInit {
 
 
   shippingAddress:Address;
+  orderDetails:any[]=[];
+
   error = '';
 
-  constructor( private _address: AddressService) { }
+  constructor( private _address: AddressService,private _product: ProductService,private _api: ApiService,) { }
 
   ngOnInit(): void {
-   
+              //get shipping address
        this._address.getAddress( this.user_address_id).subscribe(
         (res: any) => {
           
              this.shippingAddress = res.data[0];
-            
+            // console.log(res.data[0]);
         },
         (err) => {
           this.error = err.error.message;
         }
       );
+
+          //get order details and product details 
+      this._api.getTypeRequest(`orders/single/?orderId=${this.orderId}&userId=${this.user_id}`).subscribe(
+        (res: any) => {
+            
+          res.data.forEach((item) => {
+            this._product
+              .getSingleProduct(item.product_id)
+              .subscribe((product) => {
+                // console.log(product);
+                this.orderDetails.push({ ...product, ...item });
+                
+              });
+          });
+
+
+          // console.log(this.orderDetails);
+          // let uniqueProductsArray = Array.from(
+          //   new Set(res.data.map((p) => p.product_id))
+          // );
+
+        },
+        (err) => {
+          this.error = err.error.message;
+        }
+      );
+
+
+
+
+
+
+
     
   }
  
